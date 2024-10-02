6b62f78c0db0bf25041809e1bf5744f5080bae1b
Sangameshwar <sangameshwarp1105@gmail.com>
2024-10-02 19:50:26 +0530

    showing-time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(intervalId); 
+  }, []);
+
+  const secondsDegrees = (time.getSeconds() / 60) * 360 + 90;
+  const minutesDegrees = (time.getMinutes() / 60) * 360 + (time.getSeconds() / 60) * 6 + 90;
+  const hoursDegrees = (time.getHours() % 12 / 12) * 360 + (time.getMinutes() / 60) * 30 + 90;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="clock">
+        <div className="clock-face">
+          <div className="hand hour-hand" style={{ transform: `rotate(${hoursDegrees}deg)` }}></div>
+          <div className="hand minute-hand" style={{ transform: `rotate(${minutesDegrees}deg)` }}></div>
+          <div className="hand second-hand" style={{ transform: `rotate(${secondsDegrees}deg)` }}></div>
+        </div>
+      </div>
+
+      {/* Digital Clock */}
+      <div className="digital-clock">
+        {time.toLocaleTimeString()}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
